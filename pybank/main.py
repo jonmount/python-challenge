56bd4ef1a54700d4fce066c649835eaa37e1247e
Jonathan Mount <jonmount@mdscom.com>
2021-01-08 17:30:04 -0700

    Adding print statements

--- pybank/main.py
+++ pybank/main.py
@@ -1,1 +1,29 @@
-print("Pybank")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Financial Analysis")
+print("--------------------")
+print("Total Months =")
+print("Total : ")
+print("Average Change : ")
+print('Greatest Increaser in Profits :')
+print("Greatest Decrease in Profits : ")
